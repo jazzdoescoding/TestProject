4cb59ed2bc0882012060f142e0e8671de77b9a6e
jazzvibing <06652@farnborough.ac.uk>
2020-11-06 13:49:48 +0000

    test commit/push

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        String message = "howdy";
+        System.out.println(message);
+        System.out.println(message.length());
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
